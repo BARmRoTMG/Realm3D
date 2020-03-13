eeed083bbbd1c177608617ec850d863e8773ea48
BARmRoTMG <barbar903@walla.com>
2020-03-13 19:50:32 +0200

    PlayerController + Terrrain

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public GameObject player;
+
+    public float speed = 12f;
+    public float walk = 12f;
+    public float sprint = 20f;
+    public float gravity = -19.62f;
+    public float jumpHeight = 3f;
+    private bool isgolai = false;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprint;
+        } else
+        {
+            speed = walk;
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+
+        }
+        else
+        {
+
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
